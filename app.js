31525e5ea47693883fe4a702cebeb849244e2043
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-12-24 12:55:51 +0200

    Add 404 page and it's route

--- app.js
+++ app.js
@@ -17,4 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', routes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+})
+
 app.listen(3300);
