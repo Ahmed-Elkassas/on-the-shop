18391413c3e599abf86d6451e462ab7a0430e6c9
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-12-24 12:55:22 +0200

    sparate products array and data methods with new model

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,17 @@
+const Product = require("../models/product");
 
-const products = [];
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product', {pageTitle: 'Add New Product'})
 }
 
 exports.PostNewProduct =  (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product =  new Product(req.body.title);
+    product.save()
     res.redirect('/');
 };
 
 exports.getProductList = (req, res, next) => {
+    const products = Product.fetchAll()
     res.render('shop/product-list', {pageTitle: 'Product List', products})
 }
