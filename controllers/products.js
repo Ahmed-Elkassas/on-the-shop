e673d4c6d6b76f2e8d9a8b65632d8399a112f51b
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-12-25 14:26:37 +0200

    Add new views with no-content so far and Add them to navigaiton bar

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {pageTitle: 'Add New Product'})
+    res.render('admin/add-product', {pageTitle: 'Add New Product', path: '/admin/add-product'})
 }
 
 exports.PostNewProduct =  (req, res, next) => {
@@ -12,6 +12,6 @@ exports.PostNewProduct =  (req, res, next) => {
 
 exports.getProductList = (req, res, next) => {
      Product.fetchAll(products => {
-        res.render('shop/product-list', {pageTitle: 'Product List', products})
+        res.render('shop/product-list', {pageTitle: 'Product List', products, path: '/'})
      })
 }
