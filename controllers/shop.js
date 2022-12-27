2d5d68d8a4e7e546ad88a329eb3d37d9f5afdfcd
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-12-27 08:14:27 +0200

    Display cart list with initial title data only so far

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,37 +2,56 @@ const Cart = require("../models/cart");
 const Product = require("../models/product");
 
 exports.getProductList = (req, res, next) => {
-    Product.fetchAll(products => {
-       res.render('shop/product-list', { pageTitle: 'Product List', products, path: '/' })
-    })
-}
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      pageTitle: "Product List",
+      products,
+      path: "/",
+    });
+  });
+};
 
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
   Product.findById(productId, (product) => {
-    res.render('shop/product-detail', {product, pageTitle: product.title, path: '/products'})
-  })
-}
+    res.render("shop/product-detail", {
+      product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll(products => {
-      res.render('shop/index', {
-        products,
-        pageTitle: 'Shop',
-        path: '/'      
-      });
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      products,
+      pageTitle: "Shop",
+      path: "/",
     });
-  };
+  });
+};
 
 exports.getCartList = (req, res, next) => {
-    res.render('shop/cart', { pageTitle: 'Your Cart', path: 'cart' })
-}
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for(let product of products) {
+        const cartProductData = cart.products.find(item => item.id === product.id);
+        if(cartProductData) {
+          cartProducts.push({productData: product, qty: cartProductData.qty});
+        }
+      }
+    res.render("shop/cart", { pageTitle: "Your Cart", path: "/cart", products: cartProducts });
+    })
+  })
+};
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
- Product.findById(productId, product => {
-  Cart.addProduct(productId, product.price)
+  Product.findById(productId, (product) => {
+    Cart.addProduct(productId, product.price);
+  });
+  res.redirect("/cart");
+};
 
-})
- res.redirect('/cart')
-}
